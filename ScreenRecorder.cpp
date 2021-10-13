c3da5eb80a0e3825e557f3c12b3e2c9826b1362f
sofia-catalano <s290364@studenti.polito.it>
2021-10-13 11:59:21 +0200

    Add C++ project

---
+++ ScreenRecorder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sofia Catalano on 08/10/21.
+//
+
+#include "ScreenRecorder.h"
---
+++ ScreenRecorder.h
@@ -0,0 +1,23 @@
+//
+// Created by Sofia Catalano on 08/10/21.
+//
+
+#ifndef PROGETTOPDS_SCREENRECORDER_H
+#define PROGETTOPDS_SCREENRECORDER_H
+#include <iostream>
+#include <stdlib.h>
+#include "libavformat/avio.h"
+#include "libavutil/audio_fifo.h"
+#include "libavcodec/avcodec.h"
+#include "libavdevice/avdevice.h"
+#include "libavformat/avformat.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/opt.h"
+#include "libswresample/swresample.h"
+#include "libswscale/swscale.h"
+class ScreenRecorder {
+
+};
+
+
+#endif //PROGETTOPDS_SCREENRECORDER_H
